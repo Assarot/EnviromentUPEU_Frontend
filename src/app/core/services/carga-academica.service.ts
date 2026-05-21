81461fa1e6e9bd74bc4b9b105eaa2c5d14212a4c
Assarot <saemnapan@gmail.com>
2026-05-21 14:12:59 -0500

    fix url

--- src/app/core/services/carga-academica.service.ts
+++ src/app/core/services/carga-academica.service.ts
@@ -1,23 +1,24 @@
 import { HttpClient, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CargaAcademicaService {
   // Se usa esta URL por defecto de momento, sin usar el environment como se solicitó
-  private apiUrl = 'http://localhost:8080/api/v1/import/upload-excel';
+  private apiUrl = `${environment.apiUrl}/api/v1/import/upload-excel`;
 
   constructor(private http: HttpClient) {}
 
   uploadFile(file: File): Observable<HttpEvent<any>> {
     const formData = new FormData();
     formData.append('file', file);
-    
+
     return this.http.post<any>(this.apiUrl, formData, {
       reportProgress: true,
-      observe: 'events'
+      observe: 'events',
     });
   }
 }
